912562019a276f43a4ed4226a334147f413a37ae
zavbak <799000@mail.ru>
2018-08-16 16:05:19 +1000

    homework3

--- core/src/ru/geekbrains/stargame/Star2DGame.java
+++ core/src/ru/geekbrains/stargame/Star2DGame.java
@@ -3,10 +3,12 @@ package ru.geekbrains.stargame;
 
 import com.badlogic.gdx.Game;
 
+import ru.geekbrains.stargame.screen.MenuScreen;
 
 public class Star2DGame extends Game {
     @Override
     public void create() {
-        setScreen(new StarGame(this));
+        setScreen(new MenuScreen(this));
     }
 }
+
